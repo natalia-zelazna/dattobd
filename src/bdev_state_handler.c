34c2056d7b192649f0d204a16c6c80bda7947a5b
Natalia Zelazna <natalia.zelazna@datto.com>
2024-04-08 14:19:13 +0200

    test 2

--- src/bdev_state_handler.c
+++ src/bdev_state_handler.c
@@ -189,8 +189,8 @@ int handle_bdev_mount_event(const char *dir_name, int follow_flags,
         if (!(follow_flags & UMOUNT_NOFOLLOW))
                 lookup_flags |= LOOKUP_FOLLOW;
 
-#if LINUX_VERSION_CODE >= KERNEL_VERSION(5,5,0)
-//#ifdef HAVE_KERN_PATH
+// #if LINUX_VERSION_CODE >= KERNEL_VERSION(5,5,0)
+#ifdef HAVE_KERN_PATH
         ret = kern_path(dir_name, lookup_flags, &path);
 #else
         ret = user_path_at(AT_FDCWD, dir_name, lookup_flags, &path);
